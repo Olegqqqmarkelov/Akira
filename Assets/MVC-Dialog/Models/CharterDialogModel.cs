5fc9c6803a61834d6f4aa70052d486bb0c07e553
Olegqqqmarkelov <62740940+Olegqqqmarkelov@users.noreply.github.com>
2021-11-02 12:50:38 +0200

    0.01.002.02

---
+++ Assets/MVC-Dialog/Models/CharterDialogModel.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharterDialogModel : MonoBehaviour
+{
+    public int Id;
+    public string Name;
+    public string Dialog;
+}
