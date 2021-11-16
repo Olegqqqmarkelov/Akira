eb36f2eb78ecb01031f6bed88ed26c905eaa2fff
Olegqqqmarkelov <62740940+Olegqqqmarkelov@users.noreply.github.com>
2021-11-16 20:17:02 +0200

    0.01.002.07

--- Assets/MVC-Dialog/Controller/DialogueStart.cs
+++ Assets/MVC-Dialog/Controller/DialogueStart.cs
@@ -1,11 +1,29 @@
+using System;
 using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 using System.Collections.Generic;
 
+public class DialogModel
+{
+    public int id;
+    public string name;
+    public string dialog;
+}
+
 public class DialogueStart : MonoBehaviour
 {
+    public int asd = 1;
+
+    [SerializeField] private Text dialogName;
+    [SerializeField] private Text dialogText;
+    public List<DialogModel> dialogs = new List<DialogModel>();
+
+    [SerializeField] private PlayerD _playerData;
+    [SerializeField] private NPCData _npcData;
+
     [SerializeField] private GameObject dialogueLetter;
-    public OpenDialogue openDialogue;
+    [SerializeField] private OpenDialogue openDialogue;
     private bool isActive;
 
     private void OnTriggerEnter(Collider other) {
@@ -19,8 +37,41 @@ public class DialogueStart : MonoBehaviour
         {
             dialogueLetter.SetActive(false);
             isActive = false;
+
+            if(dialogs.Count == 0){
+                LoadText(_playerData.chapter);
+                WriteText(asd);
+            }else{WriteText(asd);}
+            WriteText(asd);
+            asd++;
+
             openDialogue.Open();
         }
+    }
+
+    public void LoadText(int NameFile)
+    {
+        TextAsset dialogsData = Resources.Load<TextAsset>("DialogData/Charter" + NameFile.ToString());
+
+        string[] data = dialogsData.text.Split(new char[] {'\n'});
+
+        for (int i = 0; i < data.Length; i++)
+        {
+            string[] row = data[i].Split(new char[] { ',' });
+
+            DialogModel d = new DialogModel();
+            int.TryParse(row[0], out d.id);
+            d.name = row[1];
+            d.dialog = row[2];
+
+            dialogs.Add(d);
+        }
+    }
+
+    public void WriteText(int asd)
+    {
+        dialogName.text = dialogs[asd].name;
+        dialogText.text = dialogs[asd].dialog;
     }
 
     private void OnTriggerExit(Collider other) {
---
+++ Assets/Script/NPC/NPCData.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPCData : MonoBehaviour
+{
+    public int level;
+    public string Name;
+    public int hp;
+}
