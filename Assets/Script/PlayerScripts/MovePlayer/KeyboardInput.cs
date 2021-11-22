71b94942fff5d3a73d3c16ac1a1785dd3f09bc8b
Olegqqqmarkelov <62740940+Olegqqqmarkelov@users.noreply.github.com>
2021-11-22 19:11:51 +0200

    0.01.004

--- Assets/Script/PlayerScripts/MovePlayer/KeyboardInput.cs
+++ Assets/Script/PlayerScripts/MovePlayer/KeyboardInput.cs
@@ -6,24 +6,40 @@ public class KeyboardInput : MonoBehaviour
 {
 	[SerializeField] private PhysicsMovement _movement;
     [SerializeField] private GameObject _charterSprite;
+
+    [SerializeField] private ParticleSystem particle_1;
+    [SerializeField] private ParticleSystem particle_2;
     
     [SerializeField] private GameObject _trigerForward;
     [SerializeField] private GameObject _trigerBack;
     [SerializeField] private GameObject _trigerLeft;
     [SerializeField] private GameObject _trigerRight;
     public bool moveIsActive = true;
+    private bool active = false;
 
+    void Start()
+    {
+        StopParticls();
+    }
 
     private void LateUpdate()
     {
         if(moveIsActive){
             Moves();
+
+            if(active)
+            {
+                StartParticls();
+            }else{
+                StopParticls();
+            }
         }
     }
 
     private void Moves()
     {
         Vector3 CharterScale = _charterSprite.transform.localScale;
+        active = false;
         float _speed = 2.5f;
         float vertical;
         float horizontal;
@@ -35,30 +51,48 @@ public class KeyboardInput : MonoBehaviour
 
         if(Input.GetKey(KeyCode.W) && trigerValueForward){
             vertical = 1;
+            active = true;
         } else if(Input.GetKey(KeyCode.S) && trigerValueBack){
             vertical = -1;
+            active = true;
         } else {vertical = 0;}
 
         if(Input.GetKey(KeyCode.A) && trigerValueLeft){
             horizontal = -1;
             CharterScale.x = -3;
+            active = true;
         } else if(Input.GetKey(KeyCode.D) && trigerValueRight){
             horizontal = 1;
             CharterScale.x = 3;
+            active = true;
         } else {horizontal = 0;}
 
         if(Input.GetKey(KeyCode.A) && Input.GetKey(KeyCode.D))
         {
             horizontal = 0;
+            active = false;
         }
 
         if(Input.GetKey(KeyCode.LeftShift))
         {
             _speed = 3f;
         }
+
         CharterScale.y = 3;
         _charterSprite.transform.localScale = CharterScale;
 
         _movement.Move(new Vector3(horizontal, 0, vertical), _speed);
+    }
+
+    private void StartParticls()
+    {
+        particle_1.enableEmission = true;
+        particle_2.enableEmission = true;
+    }
+
+    private void StopParticls()
+    {
+        particle_1.enableEmission = false;
+        particle_2.enableEmission = false;
     }
 }
