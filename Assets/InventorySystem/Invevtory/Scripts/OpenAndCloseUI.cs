17bee609cf20e8a24c7abdab51b00950ef91ca7e
Olegqqqmarkelov <62740940+Olegqqqmarkelov@users.noreply.github.com>
2021-12-28 20:20:15 +0200

    0.01.005.07

--- Assets/InventorySystem/Invevtory/Scripts/OpenAndCloseUI.cs
+++ Assets/InventorySystem/Invevtory/Scripts/OpenAndCloseUI.cs
@@ -6,15 +6,20 @@ public class OpenAndCloseUI : MonoBehaviour
 {
     // Start is called before the first frame update
     private Animator anim;
+    [SerializeField] private KeyboardInput _keyBoard;
 
     void Start()
     {
         anim = GetComponent<Animator>();
+        InventoryObject inventory = _keyBoard.inventory;
+
     }
 
     public void Open()
     {
         anim.SetBool("SetActive", true);
+
+
     }
 
     public void Close()
