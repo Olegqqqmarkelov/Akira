8b8f43031379bab854721ef50b30291134ebfc09
Olegqqqmarkelov <62740940+Olegqqqmarkelov@users.noreply.github.com>
2021-12-29 10:31:55 +0200

    0.01.005.08

--- Assets/InventorySystem/Invevtory/Scripts/OpenAndCloseUI.cs
+++ Assets/InventorySystem/Invevtory/Scripts/OpenAndCloseUI.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.UI;
 using UnityEngine;
 
 public class OpenAndCloseUI : MonoBehaviour
@@ -7,6 +8,9 @@ public class OpenAndCloseUI : MonoBehaviour
     // Start is called before the first frame update
     private Animator anim;
     [SerializeField] private KeyboardInput _keyBoard;
+    [SerializeField] private ScrollRect scrollRect;
+
+    private bool _isActive = false;
 
     void Start()
     {
@@ -14,16 +18,28 @@ public class OpenAndCloseUI : MonoBehaviour
         InventoryObject inventory = _keyBoard.inventory;
 
     }
-
+    
     public void Open()
     {
         anim.SetBool("SetActive", true);
-
-
+        _isActive = true;
     }
 
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.W) && _isActive)
+        {
+            scrollRect.verticalNormalizedPosition += Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.S) && _isActive)
+        {
+            scrollRect.verticalNormalizedPosition -= Time.deltaTime;
+        }
+
+    }
     public void Close()
     {
         anim.SetBool("SetActive", false);
+        _isActive = false;
     }
 }
